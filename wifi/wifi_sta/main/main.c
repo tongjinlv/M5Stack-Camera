e3a8daefbe2b27849c006ff9f28c28fceaa040b3
sanby <sanby@matalab.com>
2020-11-20 09:53:24 +0800

    over

--- wifi/wifi_sta/main/main.c
+++ wifi/wifi_sta/main/main.c
@@ -61,7 +61,6 @@ static camera_config_t camera_config = {
     .pin_xclk = CAM_PIN_XCLK,
     .pin_sscb_sda = CAM_PIN_SIOD,
     .pin_sscb_scl = CAM_PIN_SIOC,
-
     .pin_d7 = CAM_PIN_D7,
     .pin_d6 = CAM_PIN_D6,
     .pin_d5 = CAM_PIN_D5,
@@ -73,15 +72,12 @@ static camera_config_t camera_config = {
     .pin_vsync = CAM_PIN_VSYNC,
     .pin_href = CAM_PIN_HREF,
     .pin_pclk = CAM_PIN_PCLK,
-
     //XCLK 20MHz or 10MHz
     .xclk_freq_hz = CAM_XCLK_FREQ,
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
-
     .pixel_format = PIXFORMAT_JPEG,//YUV422,GRAYSCALE,RGB565,JPEG
     .frame_size = FRAMESIZE_HVGA,//QQVGA-UXGA Do not use sizes above QVGA when not JPEG
-
     .jpeg_quality = 15, //0-63 lower number means higher quality
     .fb_count = 2 //if more than one, i2s runs in continuous mode. Use only with JPEG
 };
